e037815f60b5c95e6e5021cb50a30dbe5f4f3163
JTZ18 <jon.taylorlim99@gmail.com>
2022-07-28 19:12:07 +0800

    add cors credentials

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({
     extended: true
 }))
-app.use(cors({origin: true}))
+app.use(cors({origin: true, credentials: true}))
 app.use(cookieParser());
 
 
