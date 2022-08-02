d9890de459dcc60ad7186a0a82b15d8688a8e88a
s-hin-kansen <shaun_hin@mymail.sutd.edu.sg>
2022-08-02 10:57:07 +0800

    Update userController.js

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -106,7 +106,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m' })
     const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' })
-    res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24, secure: true, sameSite: 'none'  })
+    res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 1000 * 60 * 60 * 24, secure: true, sameSite: 'none' })
     return res.status(201).json({
         success: true,
         message: 'User created successfully',
